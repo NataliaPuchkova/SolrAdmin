bafa6c093ebb9351e9d892e17d8a266248063dc6
alfalavista <natalia.puchkova@gmail.com>
2020-05-29 21:19:26 -0500

    first init

---
+++ src/main/java/alfa/house/SolrAdmin/SolrAdminApplication.java
@@ -0,0 +1,13 @@
+package alfa.house.SolrAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alfa/house/SolrAdmin/SolrAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package alfa.house.SolrAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
